9411d46e0c7d0cf3b2798dcecfb3b160bdb47bcf
Matthew Tripodi <mtripodi@F2N051.42adel.org.au>
2023-11-23 18:05:11 +1030

    ex06

--- c01/ex05/ft_putstr.c
+++ c01/ex05/ft_putstr.c
@@ -1,47 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mtripodi <mtripodi@student.42adel.org.au>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/22 21:00:26 by mtripodi          #+#    #+#             */
-/*   Updated: 2023/11/23 16:09:57 by mtripodi         ###   ########.fr       */
+/*   Created: 2023/11/23 16:11:18 by mtripodi          #+#    #+#             */
+/*   Updated: 2023/11/23 17:51:11 by mtripodi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 
-void	ft_ultimate_div_mod(int *a, int *b)
+void	ft_putstr(char *str)
 {
-	int		sum;
-	int		rem;
-	int		tempa;
-	int		tempb;
+	int		i;
 
-	tempa = *a;
-	tempb = *b;
-	sum = tempa / tempb;
-	rem = tempa % tempb;
-	*a = sum;
-	*b = rem;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
 }
-
 /*
 int	main(void)
 {
-	int n1;
-	int n2;
+	char 	s[] = "Sharks Dawwg";
 
-	n1 = 66;
-	n2 = 7;
-
-	printf("66 / 7 = 9, rn n1 = %d\n", n1);
-	printf("66 %% 7 = 9, rn n2 = %d\n", n2);
-	ft_ultimate_div_mod(&n1, &n2);
-	printf("now n1 = %d\n", n1);
-	printf("now n2 = %d\n", n2);
+	ft_putstr(s);
 	return (0);
 }
 */
--- c01/ex06/ft_strlen.c
+++ c01/ex06/ft_strlen.c
@@ -1,47 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mtripodi <mtripodi@student.42adel.org.au>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/22 21:00:26 by mtripodi          #+#    #+#             */
-/*   Updated: 2023/11/23 16:09:57 by mtripodi         ###   ########.fr       */
+/*   Created: 2023/11/23 17:47:17 by mtripodi          #+#    #+#             */
+/*   Updated: 2023/11/23 18:04:50 by mtripodi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 
-void	ft_ultimate_div_mod(int *a, int *b)
+int	ft_strlen(char *str)
 {
-	int		sum;
-	int		rem;
-	int		tempa;
-	int		tempb;
+	int		i;
 
-	tempa = *a;
-	tempb = *b;
-	sum = tempa / tempb;
-	rem = tempa % tempb;
-	*a = sum;
-	*b = rem;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
-
 /*
 int	main(void)
 {
-	int n1;
-	int n2;
-
-	n1 = 66;
-	n2 = 7;
-
-	printf("66 / 7 = 9, rn n1 = %d\n", n1);
-	printf("66 %% 7 = 9, rn n2 = %d\n", n2);
-	ft_ultimate_div_mod(&n1, &n2);
-	printf("now n1 = %d\n", n1);
-	printf("now n2 = %d\n", n2);
+	char	s[] = "SharkBaby!";
+	
+	printf("s length = %d", ft_strlen(s));
 	return (0);
 }
 */
