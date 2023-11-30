66551a5ac7f0170585e449e2701df4d919c236cf
Matthew Tripodi <mtripodi@F2N050.42adel.org.au>
2023-11-30 17:13:19 +1030

    gitap

---
+++ c06/ex01/ft_print_params.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtripodi <mtripodi@student.42adel.org.au>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/30 13:12:11 by mtripodi          #+#    #+#             */
+/*   Updated: 2023/11/30 17:12:14 by mtripodi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+	int	c;
+
+	c = 1;
+	if (argc > 1)
+	{
+		while (c < argc)
+		{
+			i = 0;
+			while (argv[c][i] != '\0')
+			{
+				write(1, &argv[c][i], 1);
+				i++;
+			}
+			write(1, "\n", 1);
+			c++;
+		}
+	}
+}
