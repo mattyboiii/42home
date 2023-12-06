fdf25a94af8a37f85257f388e3b0e5d5b1413e09
Matthew Tripodi <mtripodi@F2N043.42adel.org.au>
2023-12-06 12:29:52 +1030

    gitap

--- c05/ex01/ft_recursive_factorial.c
+++ c05/ex01/ft_recursive_factorial.c
@@ -6,7 +6,7 @@
 /*   By: mtripodi <mtripodi@student.42adel.org.au>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 18:17:52 by mtripodi          #+#    #+#             */
-/*   Updated: 2023/12/06 11:09:37 by mtripodi         ###   ########.fr       */
+/*   Updated: 2023/12/06 11:23:57 by mtripodi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,28 +21,30 @@ int	ft_recursive_factorial(int nb)
 		return (1);
 	else
 	{
-		return nb *= ft_recursive_factorial(nb - 1);
+		return (nb *= ft_recursive_factorial(nb - 1));
 	}
 }
-
+/*
 int	main(void)
 {
 	int num = 2;
 	int num2 = 3;
 	int num3 = 4;
-	int sum;
 
-	sum = num * 2 * 1;
+	num = num * 1;
 	printf("!num = %d\n", num);
-	sum = num2 * 3 * 2 * 1;
+	num2 = num2 * 2 * 1;
 	printf("!num2 = %d\n", num2);
-	sum = num3 * 4 * 3 * 2 * 1;
+	num3 = num3 * 3 * 2 * 1;
 	printf("!num3 = %d\n", num3);
 
+	num = 2;
+	num2 = 3;
+	num3 = 4;
 	printf("ft_rf 2 = %d\n", ft_recursive_factorial(num));
 	printf("ft_rf 3 = %d\n", ft_recursive_factorial(num2));
 	printf("ft_rf 3 = %d\n", ft_recursive_factorial(num3));
 
 	return (0);
 }
-
+*/
