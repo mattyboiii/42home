b0697918fc9ff48a72adfa815151bd4d4868112a
Matthew Tripodi <mtripodi@F2N042.42adel.org.au>
2023-12-07 14:45:45 +1030

    gitap

--- c07/ex02/ft_ultimate_range.c
+++ c07/ex02/ft_ultimate_range.c
@@ -6,7 +6,7 @@
 /*   By: mtripodi <mtripodi@student.42adel.org.au>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 13:42:17 by mtripodi          #+#    #+#             */
-/*   Updated: 2023/12/07 13:59:37 by mtripodi         ###   ########.fr       */
+/*   Updated: 2023/12/07 14:45:42 by mtripodi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,39 +16,39 @@
 
 int	ft_ultimate_range(int **range, int min, int max)
 {
-
-	int	i;
-	int	*out;
 	int numc;
 	
-	out = NULL;
 	numc = max - min;
-	if (min > max)
-		return (NULL);
-	out = malloc(sizeof(int) * numc);
-	i = 0;
-	while (min < max)
-		out[i++] = min++;
-	
-
+	if (min >= max)
+	{
+		*range = NULL;
+		return (0);
+	}
+	*(range) = malloc(sizeof(int) * numc);
+	if (*(range) == NULL)
+	{
+		*range = NULL;
+		return (-1);
+	}
+	else 
+	{
+		*(range) = &numc;
+		return (numc);
+	}
 }
 
 int	main(void)
 {
 	int	 min = -10;
 	int	max = 7;
-	int	**range;
-	int *dupog = ft_range(range, min, max);
+	int	*range;
+	int size = ft_ultimate_range(&range, min, max);
 
 	if (dupog != NULL)
 	{
 		int i = 0;
-		while (i < max - min)
-		{
-			printf("%d ", dupog[i++]);
-		}
+		printf("%d", out);
 		printf("\n");
-		free(dupog);
 	}
 	return (0);
 }
