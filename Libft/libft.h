1fe777769c059f5722dadb28ac06be828d340e75
Matthew Tripodi <mtripodi@F2N043.42adel.org.au>
2024-03-06 14:42:32 +1030

    gitap

---
+++ Libft/libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtripodi <mtripodi@student.42adel.org.au>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/06 12:01:31 by mtripodi          #+#    #+#             */
+/*   Updated: 2024/03/06 14:32:37 by mtripodi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+#endif
+
