914d25a46949bde66fa9c53a40ab0a340a767eba
matthew <matthew@Matthew-vm.myguest.virtualbox.org>
2024-03-14 14:40:31 +1030

    gitap

--- Libft/include/libft.h
+++ Libft/include/libft.h
@@ -25,7 +25,8 @@ int	ft_isprint(int c);
 
 size_t	ft_strlen(const char *s);
 
-void	*ft_memset(void *s, int c, size_t len);
+void	*ft_memset(void *s, int c, size_t n);
+void ft_bzero(void *s, size_t n);
 
 #endif
 
--- Libft/src/ft_memset.c
+++ Libft/src/ft_memset.c
@@ -1,13 +1,13 @@
 #include "libft.h"
 
-void	*ft_memset(void *s, int c, size_t len)
+void	*ft_memset(void *s, int c, size_t n)
 {
 	unsigned char *ptr;
 	unsigned int i;
 
 	ptr = s;
 	i = 0;
-	while (i < len)
+	while (i < n)
 	{
 		ptr[i] = (unsigned char)c;
 		i++;
