9cadcb188a2935e3d927b58aec9dfe496bdd3ae4
Matthew Tripodi <mtripodi@F3C002.42adel.org.au>
2024-03-20 19:10:32 +1030

    gitap

--- Libft/libft.h
+++ Libft/libft.h
@@ -25,6 +25,8 @@ int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int	ft_isascii(int c);
 int	ft_isprint(int c);
+int ft_memcmp(const void *s1, const void *s2, size_t n);
+int ft_strncmp(const char *s1, const char *s2, size_t n);
 
 size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
@@ -40,4 +42,3 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 
 
 #endif
-
--- Libft/src/ft_memcmp.c
+++ Libft/src/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: mtripodi <mtripodi@student.42adel.o>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 17:03:14 by mtripodi          #+#    #+#             */
-/*   Updated: 2024/03/20 17:08:44 by mtripodi         ###   ########.fr       */
+/*   Updated: 2024/03/20 18:08:37 by mtripodi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,18 @@
 
 int	 ft_memcmp(const void *s1, const void *s2, size_t n)
 {
+	unsigned char		*p1;
+	unsigned char		*p2;
+	size_t				i;
 
+	p1 = (unsigned char *)s1;
+	p2 = (unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (p1[i] != p2[i])
+			return(p1[i] - p2[i]);
+		i++;
+	}
+	return (0);
 }
