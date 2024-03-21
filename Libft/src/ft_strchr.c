64882143603b30dd33618fac49cac6ab44446688
Matthew Tripodi <mtripodi@F3C014.42adel.org.au>
2024-03-21 11:37:26 +1030

    gitap

--- Libft/src/ft_strchr.c
+++ Libft/src/ft_strchr.c
@@ -6,34 +6,37 @@
 /*   By: mtripodi <mtripodi@student.42adel.org.au>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 09:18:25 by mtripodi          #+#    #+#             */
-/*   Updated: 2024/03/20 16:59:20 by mtripodi         ###   ########.fr       */
+/*   Updated: 2024/03/21 11:37:22 by mtripodi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //DESCRIPTION
-//    The strchr() function locates the first occurrence of c (converted to a char) in the string pointed to by s.  The
-//     terminating null character is considered to be part of the string; therefore if c is `\0', the functions locate
-//     the terminating `\0'.
+//    The strchr() function locates the first occurrence of c (converted to
+//    a char) in the string pointed to by s.  The terminating null character is
+//    considered to be part of the string; therefore if c is `\0', the 
+//    functions locate the terminating `\0'.
 
 //RETURN VALUES
-//     The functions strchr() and strrchr() return a pointer to the located character, or NULL if the character does not
-//     appear in the string.
+//     The functions strchr() and strrchr() return a pointer to the located
+//     character, or NULL if the character does not appear in the string.
 
 #include "libft.h"
 
 char	*ft_strchr(const char *s, int c)
 {
-	char		*ptr;
-	size_t		i;
-	char		ch;
+	char	*ptr;
+	char	ch;
 
 	ch = (char)c;
 	ptr = (char *)s;
-	i = 0;
-	while (ptr[i] != '\0' && ptr[i] != ch)
-		i++;
-	if (ptr[i] == ch)
-		return (&ptr[i]);
+	while (*ptr != '\0')
+	{
+		if (*ptr == ch)
+			return (ptr);
+		ptr++;
+	}
+	if (ch == '\0')
+		return (ptr);
 	else
 		return (NULL);
 }
