0989c4afdff2594eddde5a07bb11d63b8c5df584
Matthew Tripodi <mtripodi@F3C026.42adel.org.au>
2024-03-28 15:30:30 +1030

    gitap

---
+++ Libft/ft_lstadd_back.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtripodi <mtripodi@student.42adel.o>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/28 15:16:17 by mtripodi          #+#    #+#             */
+/*   Updated: 2024/03/28 15:30:27 by mtripodi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Function name: ft_lstadd_back
+** ------------------------------
+** Prototype: void ft_lstadd_back(t_list **lst, t_list *new);
+**
+** Parameters:
+**    lst: The address of a pointer to the first link of a list.
+**    new: The address of a pointer to the node to be added to the list.
+**
+** Return value: None
+**
+** External functions: None
+**
+** Description:
+**    Adds the node 'new' at the end of the list.
+*/
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*node;
+
+	node = new;
+	*lst = (ft_lstlast(*lst)->next);
+	*lst = new;
+	new->next = NULL;
+}
