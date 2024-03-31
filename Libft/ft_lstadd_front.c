cd0b7ed91760ff5af3584c325b8932e5f429d5c9
matthew <matthew@Matthew-vm.myguest.virtualbox.org>
2024-03-31 13:02:57 +1030

    trying somthing with lstmap

--- Libft/ft_lstadd_front.c
+++ Libft/ft_lstadd_front.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd.c                                        :+:      :+:    :+:   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mtripodi <mtripodi@student.42adel.o>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 12:33:34 by mtripodi          #+#    #+#             */
-/*   Updated: 2024/03/28 13:43:43 by mtripodi         ###   ########.fr       */
+/*   Updated: 2024/03/31 10:55:41 by mtripodi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,20 +29,25 @@
 
 // new->content  new->next = NULL        
 
-	//*200 100
-	//*100
-	//*200 = 101
-	//*200 101
-	// new = 101
-	//*lst->next = 103
+//**lst = *200
+//*lst = 101
+//*lst->next = 102
+//*new = 103
+//*new->next = 104
+
+//*new->next = lst
+//*lst = new;
+
+//*lst = 103
+//*lst->next = 101
+//*lst->next->next = 102.
 
 // its next value to be the thing to put after
 
-void ft_lstadd_front(t_list **lst, t_list *new)
-{
-	t_list	*node;
+#include "libft.h"
 
-	node = new;
-	node->next = *lst;
-	*lst = node;
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	new->next = *lst;
+	*lst = new;
 }
--- Libft/ft_lstnew.c
+++ Libft/ft_lstnew.c
@@ -1,33 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lsnew.c                                         :+:      :+:    :+:   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mtripodi <mtripodi@student.42adel.o>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 12:01:26 by mtripodi          #+#    #+#             */
-/*   Updated: 2024/03/28 12:29:55 by mtripodi         ###   ########.fr       */
+/*   Updated: 2024/03/31 11:18:27 by mtripodi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-** Function name: ft_lstnew
-** -------------------------
-** Prototype: t_list *ft_lstnew(void *content);
-**
-** Parameters:
-**    content: The content to create the node with.
-**
-** Return value:
-**    The new node.
-**
-** External functions: malloc
-**
-** Description:
-**    Allocates (with malloc(3)) and returns a new node. The member variable
-**    'content' is initialized with the value of the parameter 'content'. The
-**    variable 'next' is initialized to NULL.
-*/
+ ** Function name: ft_lstnew
+ ** -------------------------
+ ** Prototype: t_list *ft_lstnew(void *content);
+ **
+ ** Parameters:
+ **    content: The content to create the node with.
+ **
+ ** Return value:
+ **    The new node.
+ **
+ ** External functions: malloc
+ **
+ ** Description:
+ **    Allocates (with malloc(3)) and returns a new node. The member variable
+ **    'content' is initialized with the value of the parameter 'content'. The
+ **    variable 'next' is initialized to NULL.
+ */
 
 // ft_lstnew(content);
 // *new_node.content == node->content
@@ -35,9 +35,9 @@
 
 #include "libft.h"
 
-t_list *ft_lstnew(void *content)
+t_list	*ft_lstnew(void *content)
 {
-	t_list	node;
+	t_list	*node;
 
 	node = malloc(sizeof(t_list));
 	if (node == NULL)
