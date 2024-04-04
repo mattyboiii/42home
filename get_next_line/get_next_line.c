990c42e41501d2185f9d310d4cdc1c1c045871fa
matthew <matthew@Matthew-vm.myguest.virtualbox.org>
2024-04-04 11:25:36 +1030

    gitap

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,8 +6,33 @@
 /*   By: mtripodi <mtripodi@student.42adel.org.au>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 11:10:32 by mtripodi          #+#    #+#             */
-/*   Updated: 2024/04/04 11:10:34 by mtripodi         ###   ########.fr       */
+/*   Updated: 2024/04/04 11:24:19 by mtripodi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+** Function name: get_next_line
+** -----------------------------
+** Prototype: char *get_next_line(int fd);
+**
+** Parameters:
+**    fd: The file descriptor to read from.
+**
+** Return value:
+**    Read line: correct behavior
+**    NULL: there is nothing else to read, or an error occurred.
+**
+** External functions: read, malloc, free
+**
+** Description:
+**    Write a function that returns a line read from a file descriptor.
+*/
+
+/*
+** ssize_t read(int fildes, void *buf, size_t nbyte);
+**
+** DESCRIPTION
+**     read() attempts to read nbyte bytes of data from the object referenced
+**     by the descriptor fildes into the buffer pointed to by buf.
+*/
 
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,8 +6,15 @@
 /*   By: mtripodi <mtripodi@student.42adel.org.au>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 11:11:33 by mtripodi          #+#    #+#             */
-/*   Updated: 2024/04/04 11:14:34 by mtripodi         ###   ########.fr       */
+/*   Updated: 2024/04/04 11:23:06 by mtripodi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE
+
+char *get_next_line(int fd);
+
+
+
+#endif
