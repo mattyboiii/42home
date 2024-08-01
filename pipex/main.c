34ff5d0c7b98c4564bc48f0346b59cd838b34d67
matthew <matthew@Matthew-vm.myguest.virtualbox.org>
2024-08-02 09:15:50 +0930

    main.c

--- pipex/main.c
+++ pipex/main.c
@@ -6,7 +6,7 @@
 /*   By: mtripodi <mtripodi@student.42adel.o>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 08:26:28 by mtripodi          #+#    #+#             */
-/*   Updated: 2024/08/02 08:54:49 by mtripodi         ###   ########.fr       */
+/*   Updated: 2024/08/02 09:14:32 by mtripodi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,15 @@
 // 0 to 'id' from the child process
 int main()
 {
-	int id = fork();
-	printf("Current ID: %d\nParent ID: %d\n", getpid(), getppid());
-	printf("Int id: %d\n", id);
-	if (id != 0)
+	int fid = fork();
+	if (fid != 0)
+	{
 		wait(NULL);
+		printf("--Parent Process--\n");
+	}
+	else 
+		printf("--Child Process--\n");
+	printf("Process ID: %d\nParent ID: %d\n", getpid(), getppid());
+	printf("fid: %d\n\n", fid);
 	return(0);
-}j
+}
