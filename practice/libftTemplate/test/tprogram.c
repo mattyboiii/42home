524388571ca874f883fb144a48e5e05d135c6032
matthew <matthew@Matthew-vm.myguest.virtualbox.org>
2024-09-04 10:47:51 +0930

    updating completed projects and libft

---
+++ practice/libftTemplate/test/tprogram.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+#include <fcntl.h>
+#include "get_next_line.h"
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+
+	fd = 0;
+	fd = open("test/test.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		perror("Error opening file");
+		return (1);
+	}
+	line = get_next_line(fd);
+	while (line != NULL)
+	{
+		printf("%s", line);
+		free(line);
+		line = get_next_line(fd);
+	}
+	return (0);
+}
