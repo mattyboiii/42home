6d601022d96f2be52635bbec0c048d4bd9c32abf
matthew <matthew@Matthew-vm.myguest.virtualbox.org>
2024-10-17 12:12:09 +1030

    finished sort 3, Moving on to sort 5

--- push_swap/src/utils/logicfunk.c
+++ push_swap/src/utils/logicfunk.c
@@ -30,35 +30,26 @@ void	big_small_set(t_node **lst, t_node **big, t_node **small)
 	*lst = head;
 }
 
-void	double_action(t_node **lst, t_node *big, t_node *small)
-{
-	t_node	*head;
-
-	if (small->pos == 0 && big->pos == 1)
-	{
-		s(lst, 1);
-		r(lst, 1);
-	}
-	else
-	{
-		s(lst, 1);
-		rrs(lst, 1);
-	}
-}
 void	sort3(t_node **a)
 {
 	t_node		*small;
 	t_node		*big;
 
 	big_small_set(a, &big, &small);
-	if (small)
+	if (small->pos == 1 && big->pos == 2)
+		s(a, 1);
+	else if (small->pos == 1 && big->pos == 0)
+		r(a, 1);
+	else if (small->pos == 2 && big->pos == 1)
+		rrs(a, 1);
+	else if (small->pos == 0 && big->pos == 1)
 	{
-		if (small->pos == 1 && big->pos == 2)
-			s(a, 1);
-		else if (small->pos == 1 && big->pos == 0)
-			r(a, 1);
-		else if (small->pos == 2 && big->pos == 1)
-			rrs(a, 1);
+		s(a, 1);
+		r(a, 1);
 	}
-	double_action(a, big, small);
+	else
+	{
+		s(a, 1);
+		rrs(a, 1);
+	}
 }
